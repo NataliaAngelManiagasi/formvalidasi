486483c8b03543bd632b7518eb591ab0bb978ddf
NataliaAngelManiagasi <52839520+NataliaAngelManiagasi@users.noreply.github.com>
2019-07-16 16:32:36 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,34 @@
+<?php
+if (isset($_GET['nama']))
+{
+   echo $_GET['nama'];
+}
+  
+echo "<br />";
+  
+if (isset($_GET['alamat']))
+{
+   echo $_GET['alamat'];
+}
+
+echo "<br />";
+  
+if (isset($_GET['TTL']))
+{
+   echo $_GET['TTL'];
+}
+
+echo "<br />";
+  
+if (isset($_GET['jenis kelamin']))
+{
+   echo $_GET['jenis kelamin'];
+}
+
+echo "<br />";
+  
+if (isset($_GET['usia']))
+{
+   echo $_GET['usia'];
+}
+?>
